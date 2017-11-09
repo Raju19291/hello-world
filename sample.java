e5088bc691ebcd2c64dbc0ec2e931cf1ae07f56d
Your Name <raju19291@gmail.com>
2017-11-09 01:05:20 -0600

    commit1

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-        System.exit( 0 ); //success
+        System.out.println( "12345" );
+	System.exit( 0 ); //success
     }
 }
